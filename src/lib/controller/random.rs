b5a38cff723f1389f7e69bb88da31629d0f59b15
JOLIMAITRE Matthieu <matthieu@imagevo.fr>
2022-04-04 17:32:21 +0300

    added constructor to the random controller

--- src/lib/controller/random.rs
+++ src/lib/controller/random.rs
@@ -5,6 +5,12 @@ use crate::lib::grid::Grid;
 
 pub struct RandomController;
 
+impl RandomController {
+	pub fn new() -> Self {
+		Self
+	}
+}
+
 impl Controller for RandomController {
 	fn next_move(&mut self, _grid: &Grid) -> Result<Move, ControllerError> {
 		let movement = random();
