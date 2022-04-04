63d8fe2470ce09dc0ef3e75cb2349e50cee29d79
JOLIMAITRE Matthieu <matthieu@imagevo.fr>
2022-04-04 18:42:13 +0300

    better module structure, and added prelude

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -7,3 +7,13 @@ pub mod grid_displayer;
 pub fn clear_term() {
 	print!("\x1B[2J\x1B[1;1H");
 }
+
+pub mod prelude {
+	pub use super::controller::{
+		Controller, PlayerController, RandomController, SimulatedController,
+	};
+	pub use super::game::GameError;
+	pub use super::game::Rules as GameRules;
+	pub use super::game_manager::GameManager;
+	pub use super::game_manager::Rules as ManagerRules;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use lib::{
-	controller::{player::PlayerController, random::RandomController, Controller},
-	game::{self, GameError},
-	game_manager::{self, GameManager},
-};
-
 pub mod lib;
 
+use lib::prelude::*;
+
 fn main() -> Result<(), GameError> {
-	let game_rules = game::Rules::default().size(8).spawn_per_turn(1);
-	let manager_rules = game_manager::Rules::default();
+	let game_rules = GameRules::default().size(8).spawn_per_turn(1);
+	let manager_rules = ManagerRules::default();
 	let controller = PlayerController::new().into_box();
 	let mut managed = GameManager::new(game_rules, manager_rules, controller);
 	managed.play_all()
